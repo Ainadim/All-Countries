01b52a3b21d950ed1e96e052c0e37297a1a76952
Arafin Islam <nadimface@gmail.com>
2022-04-23 15:39:13 +0600

    Doee

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -5,6 +5,7 @@ const Countries = () => {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
+      // fetch("https://restcountries.com/v2/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
@@ -14,8 +15,13 @@ const Countries = () => {
       {countries.map((country) => {
         return (
           <div className="country-details">
+            {/* <h5>Name: {country.name}</h5>
+            <h6>Language: {country.languages[0].name}</h6> */}
+
             <h5>Name: {country.name.common}</h5>
-            {/* <h6>Language: {country.languages.spa}</h6> */}
+            <h6>Capital: {country.capital}</h6>
+            {/* <h6>Language: {country.languages['spa']}</h6> */}
+
             <img src={country.flags.png} alt="Flags" />
           </div>
         );
