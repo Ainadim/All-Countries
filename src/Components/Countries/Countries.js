47af861f5b13e322270fc30d7e3a6bf66db83e2e
Arafin Islam <nadimface@gmail.com>
2022-04-23 19:52:00 +0600

    Dnne

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -14,12 +14,20 @@ const Countries = () => {
       <h1>There are total {countries.length} Countries</h1>
       <div className="all-country">
         {countries.map((country) => {
+          console.log(country.languages);
           return (
             <div className="country-details">
               {/* <h5>Name: {country.name}</h5>
             <h6>Language: {country.languages[0].name}</h6> */}
 
               <h5>Name: {country.name.common}</h5>
+              {country.languages &&
+                Object.values(country.languages).map((lan) => (
+                  <>
+                    <span>{lan}</span>
+                    <br />
+                  </>
+                ))}
               <h6>Capital: {country.capital}</h6>
               {/* <h6>Language: {country.languages}</h6> */}
 
